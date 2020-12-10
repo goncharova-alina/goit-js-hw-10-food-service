bdbfec0e47f9085c241ae8a3c8b80a5498ba788d
Alina Goncharova <goncarovaalina206@gmail.com>
2020-12-10 11:16:30 +0200

    Updates

--- src/js/markup.js
+++
@@ -1,9 +0,0 @@
-import template from '../templates/gallery-item.hbs';
-import foods from '../menu.json';
-
-const galleryRef = document.querySelector('.js-menu');
-// console.log(galleryRef)
-
-const markup = template(foods);
-
-galleryRef.insertAdjacentHTML('beforeend', markup);
